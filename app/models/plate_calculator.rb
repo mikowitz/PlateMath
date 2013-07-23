3caf5abdbf1ee37028ab578228831c3b9ec4ad55
Michael Berkowitz <michael.berkowitz@gmail.com>
2013-07-23 17:37:44 -0400

    add plate calculator model and specs

---
+++ app/models/plate_calculator.rb
@@ -0,0 +1,34 @@
+class PlateCalculator
+  BAR_WEIGHT = 45
+  PLATE_WEIGHTS = [45, 35, 25, 10, 5, 2.5, 1.25]
+
+  attr_accessor :plate_counts
+  def initialize(bar_weight=nil, plate_counts=nil)
+    @bar_weight = bar_weight || BAR_WEIGHT
+    @plate_counts = plate_counts || Hash[PLATE_WEIGHTS.map{|w| [w, 10]}]
+  end
+
+  def plates(weight)
+    weight = (weight - @bar_weight) / 2.0
+    results = []
+
+    PLATE_WEIGHTS.each do |plate_weight|
+      @plate_counts[plate_weight].times do
+        if weight >= plate_weight
+          results << plate_weight
+          weight -= plate_weight
+        end
+        break if weight == 0
+      end
+    end
+    results
+  end
+
+  def set_plate_count(plate_weight, count)
+    @plate_counts = @plate_counts.merge(plate_weight => count)
+  end
+
+  def set_bar_weight(bar_weight)
+    @bar_weight = bar_weight
+  end
+end
---
+++ spec/plate_calculator_spec.rb
@@ -0,0 +1,23 @@
+describe PlateCalculator do
+  before do
+    @calculator = PlateCalculator.new
+  end
+
+  it 'should calculate correctly with unlimited plates' do
+    @calculator.plates(45).should == []
+    @calculator.plates(50).should == [2.5]
+    @calculator.plates(85).should == [10, 10]
+    @calculator.plates(100).should == [25, 2.5]
+    @calculator.plates(102.5).should == [25, 2.5, 1.25]
+  end
+
+  it 'should calculate corectly with limited plates' do
+    @calculator.set_plate_count(10, 1)
+    @calculator.plates(85).should == [10, 5, 5]
+  end
+
+  it 'should calculate correctly with a different bar weight' do
+    @calculator.set_bar_weight(25)
+    @calculator.plates(85).should == [25, 5]
+  end
+end
